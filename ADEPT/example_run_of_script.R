bd635a9c34809bcf52f5f2341b23fb10366521e5
tnecamp <tnecamp@gmail.com>
2017-09-03 17:08:00 -0400

    Update and rename how_to_use_code.R to example_run_of_script.R

--- ADEPT/example_run_of_script.R
+++ ADEPT/example_run_of_script.R
@@ -1,6 +1,9 @@
 ## First make sure you are in the same working directory as the data set
 data_set = read.csv("sample_data_ADEPT")
 
+# Next, make sure you have read in the function ADEPT_regression from the 
+# adept_estimation.R file
+
 ## regressing using all covariates
 regression_output1 = ADEPT_regression(data_set, "Y", "A1_ind", "response", "A2_ind", covariates=c("covariate_1", "covariate_2", "covariate_3"), "cluster.id")  ## note these names match the names in the data set
 
